f0d7ddf2b02ad10fb216aeb5fcbdc60c3a966616
percy002 <percy00261@gmail.com>
2024-08-22 17:25:30 -0500

    modificaion de pagina principal

--- front-page.php
+++ front-page.php
@@ -1,63 +1,103 @@
 <?php get_header(); ?>
 
-<?php while(have_posts()): the_post(); ?>
-    
-    <?php 
-        if (get_field('portada_1')) $portada1 = get_field('portada_1');
-        if (get_field('abstract')) $abstract = get_field('abstract');
-        if (get_field('imagen_taller')) $imagen_taller = get_field('imagen_taller');
-        if (get_field('catalogo1')) $catalogo1 = get_field('catalogo1');
-        if (get_field('catalogo2')) $catalogo2 = get_field('catalogo2');
-        if (get_field('catalogo3')) $catalogo3 = get_field('catalogo3');
-        if (get_field('video')) $video = get_field('video');
+<?php while (have_posts()):
+    the_post(); ?>
 
-        // Use preg_match to find iframe src.
-        preg_match('/src="(.+?)"/', $video, $matches);
-        $src = $matches[1];
+    <?php
+    if (get_field('portada_1'))
+        $portada1 = get_field('portada_1');
+    if (get_field('abstract'))
+        $abstract = get_field('abstract');
+    if (get_field('imagen_taller'))
+        $imagen_taller = get_field('imagen_taller');
+    if (get_field('catalogo1'))
+        $catalogo1 = get_field('catalogo1');
+    if (get_field('catalogo2'))
+        $catalogo2 = get_field('catalogo2');
+    if (get_field('catalogo3'))
+        $catalogo3 = get_field('catalogo3');
+    if (get_field('video'))
+        $video = get_field('video');
 
-        // Add extra parameters to src and replace HTML.
-        $params = array(
-            'controls'  => 0,
-            'rel'=> 0,
-            'showinfo'=> 0
-        );
-        $new_src = add_query_arg($params, $src);
-        $video = str_replace($src, $new_src, $video);
-        
+    // Use preg_match to find iframe src.
+    preg_match('/src="(.+?)"/', $video, $matches);
+    $src = $matches[1];
+
+    // Add extra parameters to src and replace HTML.
+    $params = array(
+        'controls' => 0,
+        'rel' => 0,
+        'showinfo' => 0
+    );
+    $new_src = add_query_arg($params, $src);
+    $video = str_replace($src, $new_src, $video);
+
 
     ?>
-     <div class="slider hero-home">
+    <div class="slider hero-home">
         <ul class="slides">
             <li>
-                <img src="<?php echo $portada1['url'] ?>"> 
-                <!-- <div class="caption center-align">
-                <h3>This is our big Tagline!</h3>
-                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
-                </div> -->
+                <img src="<?php echo $portada1['url'] ?>">
             </li>
         </ul>
     </div>
-    <section>
-        <div class="container about-us">
-            <p>
-                <?php 
+    <section class="pt-[120px] pb-[80px] bg-gris ">
+        <div class="flex flex-col gap-[30px] px-5 sm:px-[180px] about-us items-center">
+            <div class="flex justify-center">
+                <img class="responsive-img" src="<?php echo get_template_directory_uri(); ?>\assets\img\logo-qataya.png"
+                    alt="logo qataya">
+            </div>
+            <div class="">
+                <?php
                 echo $abstract
-                ?>
-            </p>
+                    ?>
+            </div>
+
+            <div class="max-w-[1080px] flex flex-col md:flex-row gap-20">
+                <div class="p-[20px] flex-col gap-y-20 bg-white shadow-lg rounded-lg">
+                    <h4 class="italic font-semibold">Misión</h4>
+                    <p class="text-center">Producir y comercializar productos y servicios de excelencia con tradición,
+                        creando valor en cada agente
+                        de la cadena comercial con responsabilidad social y sostenibilidad.</p>
+                </div>
+                <div class="p-[20px] flex-col gap-y-20 bg-white shadow-lg rounded-lg">
+                    <h4 class="italic font-semibold">Vision</h4>
+                    <p class="text-center">Producir y comercializar productos y servicios de excelencia con tradición,
+                        creando valor en cada agente
+                        de la cadena comercial con responsabilidad social y sostenibilidad.</p>
+                </div>
+                <div class="p-[20px] flex-col gap-y-20 bg-white shadow-lg rounded-lg">
+                    <h4 class="italic font-semibold">Valores</h4>
+                    <p class="text-center">Producir y comercializar productos y servicios de excelencia con tradición,
+                        creando valor en cada agente
+                        de la cadena comercial con responsabilidad social y sostenibilidad.</p>
+                </div>
+            </div>
+            <div class="flex justify-center">
+                <button class="bg-brown-light text-white font-semibold py-[15px] px-[30px] focus:bg-brown-light">CONOCER
+                    MAS</button>
+            </div>
         </div>
     </section>
-    <section>
-        <div class="collections container row">
+    <!--  -->
+    <section class="relative bg-tejido bg-cover bg-center py-[50px] flex flex-col gap-y-10">
+        <div class="absolute inset-0 bg-black opacity-30"></div>
+
+        <div class="z-10">
+            <h3 class="text-[70px] font-dancing text-white">Nuestras colecciones</h3>
+        </div>
+        <div class="collections container row ">
             <div class="col s12 m4">
                 <div class="card z-depth-0">
                     <div class="card-image collection-image">
-                        <a href="<?php echo get_site_url() ?>/coleccion-deep-in-nature/"><img class="responsive-image" src="<?php  echo $catalogo1['imagen1']['url'] ; ?>" alt=""></a> 
+                        <a href="<?php echo get_site_url() ?>/coleccion-deep-in-nature/"><img class="responsive-image"
+                                src="<?php echo $catalogo1['imagen1']['url']; ?>" alt=""></a>
                     </div>
                     <div class="card-content">
                         <a href="<?php echo get_site_url() ?>/coleccion-deep-in-nature/">
-                            <h4><?php 
-                                echo $catalogo1['titulo']
-                            ?></h4>
+                            <h4><?php
+                            echo $catalogo1['titulo']
+                                ?></h4>
                         </a>
                     </div>
                 </div>
@@ -65,13 +105,14 @@
             <div class="col s12 m4">
                 <div class="card z-depth-0">
                     <div class="card-image collection-image">
-                        <a href="<?php echo get_site_url() ?>/coleccion-ocaso/"><img class="responsive-image" src="<?php  echo $catalogo2['imagen1']['url'] ; ?>" alt=""></a> 
+                        <a href="<?php echo get_site_url() ?>/coleccion-ocaso/"><img class="responsive-image"
+                                src="<?php echo $catalogo2['imagen1']['url']; ?>" alt=""></a>
                     </div>
                     <div class="card-content">
                         <a href="<?php echo get_site_url() ?>/coleccion-ocaso/">
-                            <h4><?php 
-                                echo $catalogo2['titulo']
-                            ?></h4>
+                            <h4><?php
+                            echo $catalogo2['titulo']
+                                ?></h4>
                         </a>
                     </div>
                 </div>
@@ -79,53 +120,75 @@
             <div class="col s12 m4">
                 <div class="card z-depth-0">
                     <div class="card-image collection-image">
-                        <a href="<?php echo get_site_url() ?>/coleccion-happy-life/"><img class="responsive-image" src="<?php  echo $catalogo3['imagen1']['url'] ; ?>" alt=""></a> 
+                        <a href="<?php echo get_site_url() ?>/coleccion-happy-life/"><img class="responsive-image"
+                                src="<?php echo $catalogo3['imagen1']['url']; ?>" alt=""></a>
                     </div>
                     <div class="card-content">
                         <a href="<?php echo get_site_url() ?>/coleccion-happy-life/">
-                            <h4><?php 
-                                echo $catalogo3['titulo']
-                            ?></h4>
+                            <h4><?php
+                            echo $catalogo3['titulo']
+                                ?></h4>
                         </a>
                     </div>
                 </div>
             </div>
-            
-            
-            
-            
-            
+        </div>
+        <div class="flex justify-center Z-10">
+            <button class="bg-brown-light text-white font-semibold py-[15px] px-[30px] focus:bg-brown-light Z-10">VER MAS
+                PRODUCTOS</button>
         </div>
     </section>
-    <section>
-        <div class="video-container container">
-            <?php if(get_field('video')): ?>
-                <?php echo $video ?>
-            <?php endif ?>
+    <section class="py-[60px] bg-gris w-full">
+        <div class="flex w-full items-center h-[80vh]">
+            <div class="hidden sm:block bg-brown-light w-2/12 h-1/2">
+
+            </div>
+            <div class="w-full sm:w-6/12">
+                <lite-youtube videoid="Va7AqGifX74"></lite-youtube>
+
+                <!-- <?php if (get_field('video')): ?>
+                    <?php echo $video ?>
+                <?php endif ?> -->
+            </div>
+            <div class="relative bg-tejido-espiral bg-cover bg-center w-4/12 h-[80%] hidden sm:block">
+                <div class="absolute inset-0 bg-black opacity-50"></div>
+                <div class="h-full flex flex-col px-[30px] justify-center">
+                    <div class="flex flex-col gap-1 z-10 ">
+                        <p class="text-[48px] text-white text-pretty">PRODUCTOS DE ALTA CALIDAD</p>
+                        <p class="text-[40px] text-[#C8C8C8]">PARA OFRECERTE</p>
+                    </div>
+                    <div class="flex justify-center z-10">
+                        <button
+                            class="bg-brown-light text-white font-semibold py-[15px] px-[30px] focus:bg-brown-light Z-10">VER
+                            MAS
+                            PRODUCTOS</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
-    <section>
+    <section class="">
         <div class="container">
             <h3>
-                <?php if(get_locale()=='en_US'): ?>
+                <?php if (get_locale() == 'en_US'): ?>
                     Get In Touch With Us
                 <?php else: ?>
                     Pongase en Contacto Con Nosotros
                 <?php endif ?>
-                
+
             </h3>
             <div class="contact-qataya">
                 <?php get_template_part("template-parts/contact_form") ?>
             </div>
         </div>
     </section>
-    
-    
+
+
     <!-- <section>
         <div class="container double abstract">
             <div class="abstract__text">
                 <h2>
-                    <?php if(get_locale()=='en_US'): ?>
+                    <?php if (get_locale() == 'en_US'): ?>
                         THREADS OF HISTORY: THE GRAND TRADITION OF THE ART OF SILK
                     <?php else: ?>
                         THREADS OF HISTORY: THE GRAND TRADITION OF THE ART OF SILK
@@ -136,7 +199,7 @@
                     <p>
                         
                     <a href="">
-                        <?php if(get_locale()=='en_US'): ?>
+                        <?php if (get_locale() == 'en_US'): ?>
                             Discover more
                         <?php else: ?>
                             Ver mas
@@ -146,11 +209,12 @@
                 </p>
             </div>
             <div class="abstract__img">
-                <img src="<?php if (get_field('imagen_taller')) echo $imagen_taller['url'] ?>" alt="">
+                <img src="<?php if (get_field('imagen_taller'))
+                    echo $imagen_taller['url'] ?>" alt="">
             </div>
         </div>
     </section> -->
-    
-    
+
+
 <?php endwhile; ?>
 <?php get_footer() ?>
--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,41 +9,93 @@
     <script src="https://kit.fontawesome.com/841e236107.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet">
+
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script type="module" src="https://cdn.jsdelivr.net/npm/@justinribeiro/lite-youtube@1.5.0/lite-youtube.js"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        gris: '#ECECEC',
+                        'brown-light': '#CCA57E',
+
+                    },
+                    backgroundImage: {
+                        tejido: "url('<?php echo get_template_directory_uri(); ?>/assets/img/bg/bg-tejido.webp')",
+                        'tejido-espiral': "url('<?php echo get_template_directory_uri(); ?>/assets/img/bg/tejido_espiral.jpg')",
+                    },
+                    fontFamily: {
+                        dancing: ['"Dancing Script"', 'sans-serif'],
+                    },
+
+                }
+            }
+        }
+    </script>
+
     <?php wp_head(); ?>
 </head>
-<body>
-    <?php 
-        $args2=array(
-            'theme_location' => 'menu-principal',
-            'container' => '',
-            'menu_id'=>'slide-out',
-            'menu_class'=> 'sidenav',
-            // 'items_wrap' => '<ul class="sidenav collapsible" id="%1$s">%3$s</ul>'
-        );
 
-        wp_nav_menu($args2);
+<body>
+    <?php
+    $args2 = array(
+        'theme_location' => 'menu-principal',
+        'container' => '',
+        'menu_id' => 'slide-out',
+        'menu_class' => 'sidenav',
+        // 'items_wrap' => '<ul class="sidenav collapsible" id="%1$s">%3$s</ul>'
+    );
+
+    wp_nav_menu($args2);
     ?>
 
     <header>
         <!-- Dropdown Structure -->
-    
-    <div class="navbar-fixed">
+
+        <!-- <div class="navbar-fixed">
         <nav>
             <div class="nav-wrapper container">
                 <a class="brand-logo " ><img class="responsive-img" src="<?php echo get_template_directory_uri(); ?>\assets\img\logo-qataya.png" alt="logo qataya">
                 </a>
                 <a href="#" data-target="slide-out" class="top-nav right sidenav-trigger"><i class="fas fa-bars fa-2x"></i></a>
-                <?php 
-                    $args=array(
-                        'theme_location' => 'menu-principal',
-                        'container' => '',
-                        'menu_id'=> 'main-menu',
-                        'menu_class'=> 'right hide-on-med-and-down '
-                    );
-                    wp_nav_menu($args);
+                <?php
+                $args = array(
+                    'theme_location' => 'menu-principal',
+                    'container' => '',
+                    'menu_id' => 'main-menu',
+                    'menu_class' => 'right hide-on-med-and-down '
+                );
+                wp_nav_menu($args);
                 ?>
             </div>
         </nav>
-    </div>
-    
-</header>
+    </div> -->
+
+        <div class="navbar-fixed">
+            <nav>
+                <div class="nav-wrapper container">
+                    <a class="brand-logo "><img class="responsive-img"
+                            src="<?php echo get_template_directory_uri(); ?>\assets\img\logo-qataya.png"
+                            alt="logo qataya">
+                    </a>
+                    <a href="#" data-target="slide-out" class="top-nav right sidenav-trigger"><i
+                            class="fas fa-bars fa-2x"></i></a>
+                    <?php
+                    $args = array(
+                        'theme_location' => 'menu-principal',
+                        'container' => '',
+                        'menu_id' => 'main-menu',
+                        'menu_class' => 'right hide-on-med-and-down '
+                    );
+                    wp_nav_menu($args);
+                    ?>
+                </div>
+            </nav>
+        </div>
+
+    </header>
