a60703e26921dc57adfdea56b49a97b4476f9ab7
percy002 <percy00261@gmail.com>
2024-08-27 16:47:11 -0500

    modificar pagina de colecciones

--- collection.php
+++ collection.php
@@ -1,26 +1,36 @@
-<?php 
+<?php
 /*
     Template Name: pagina Colecciones
 */
 get_header(); ?>
 
-<?php while(have_posts()): the_post(); ?>
-    <?php 
-        
-        $datos_principales = get_field('datos_principales');
+<?php while (have_posts()):
+    the_post(); ?>
+    <?php
+
+    $datos_principales = get_field('datos_principales');
+    $term = get_term($datos_principales['collection_name']);
+
     ?>
-    <?php if($datos_principales):?>
-        <div>
-            <h1>
-                <?php                 
-                    $term = get_term($datos_principales['collection_name']);
-                    echo $term->name; // Muestra el nombre o etiqueta de la taxonomía
-                ?>
-            </h1>
-            
+    <?php if ($datos_principales): ?>
+        <div class="relative h-[350px] bg-prenda bg-center">
+            <div class="absolute inset-0 bg-black opacity-30"></div>
+
+            <div class="h-full flex flex-col justify-center items-center font-playfair italic">
+                <div class="flex justify-center">
+                    <h1 class="z-10 text-white text-[60px] font-playfair italic">Colección</h1>
+                </div>
+                <div class="flex gap-10 text-white z-10 text-[24px]">
+                    <span>Inicio</span>
+                    <span>Colecciones</span>
+                </div>
+            </div>
         </div>
-        <section>
-            <div class="container">
+        <section class="bg-stone flex flex-col gap-[40px]">
+            <div class="flex justify-center">
+                <h1 class="z-10 text-primary text-[60px] font-playfair italic"><?php echo $term->name; ?></h1>
+            </div>
+            <div class="px-10 lg:px-[150px]">
                 <?php list_products_taxonomy($term) ?>
             </div>
         </section>
--- inc/queries.php
+++ inc/queries.php
@@ -1,100 +1,103 @@
-<?php function list_products($category="",$cantidad="6"){?>
-        <?php 
-            $args=array(
-                "post_type"=>"products",
-                'post-page' => $cantidad,//cantidad de tours recuperados
-                'orderby' => 'modified',
-                'category_name'=> $category
-            ) ;
-            $products = new WP_Query($args);   
-            
-        ?>
-        
-        <div class="row">
-            <?php while($products->have_posts()): $products->the_post(); ?>
-                <?php 
-                    $main_image=get_field("imagenes");
-                    $productName=get_field('nombre_producto');
-                    $details = get_field("descripcion");
-                ?>
-                <div class="col s12 m3">
+<?php function list_products($category = "", $cantidad = "6")
+{ ?>
+    <?php
+    $args = array(
+        "post_type" => "products",
+        'post-page' => $cantidad,//cantidad de tours recuperados
+        'orderby' => 'modified',
+        'category_name' => $category
+    );
+    $products = new WP_Query($args);
 
-                    <div class="card">
-                        <div class="card-image">
-                            <a href="<?php echo get_permalink() ?>">
-                                <img class="" src="<?php echo $main_image['imagen1']['url'] ?>" alt="">
-                        
-                            </a>
-                        </div>
-                        <div class="card-content">
-                            <a href="<?php echo get_permalink() ?>">
-                                <h4><?php echo $productName ?></h4>
-                            </a>
-                        </div>
+    ?>
+
+    <div class="row">
+        <?php while ($products->have_posts()):
+            $products->the_post(); ?>
+            <?php
+            $main_image = get_field("imagenes");
+            $productName = get_field('nombre_producto');
+            $details = get_field("descripcion");
+            ?>
+            <div class="col s12 m3">
+
+                <div class="card">
+                    <div class="card-image">
+                        <a href="<?php echo get_permalink() ?>">
+                            <img class="" src="<?php echo $main_image['imagen1']['url'] ?>" alt="">
+
+                        </a>
+                    </div>
+                    <div class="card-content">
+                        <a href="<?php echo get_permalink() ?>">
+                            <h4><?php echo $productName ?></h4>
+                        </a>
                     </div>
                 </div>
-                <?php endwhile ?>
-                <?php wp_reset_postdata() ?>
-        </div>
+            </div>
+        <?php endwhile ?>
+        <?php wp_reset_postdata() ?>
+    </div>
 
 
 <?php } ?>
 
-<?php function list_products_taxonomy($collection=""){?>
-        <?php 
-            $args=array(
-                "post_type"=>"products",
-                'posts_per_page' => -1, 
-                'orderby' => 'modified',
-                'tax_query' => array(
-                    array(
-                        'taxonomy' => 'collection', // Reemplaza 'nombre_de_la_taxonomia' por el nombre de tu taxonomía personalizada
-                        'field' => 'slug', // Puedes usar 'term_id', 'slug' o 'name' como campo de comparación
-                        'terms' => $collection->slug, // Reemplaza 'nombre_del_termino' por el término específico de la taxonomía que deseas consultar
-                    ),
-                ),
-                
-            ) ;
-            $products = new WP_Query($args);   
-            
-        ?>
-        <?php if($products->have_posts()): ?>
-            <div class="row">
-                <div class="col s12">
-                    <div class="row">
+<?php function list_products_taxonomy($collection = "")
+{ ?>
+    <?php
+    $args = array(
+        "post_type" => "products",
+        'posts_per_page' => -1,
+        'orderby' => 'modified',
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'collection', // Reemplaza 'nombre_de_la_taxonomia' por el nombre de tu taxonomía personalizada
+                'field' => 'slug', // Puedes usar 'term_id', 'slug' o 'name' como campo de comparación
+                'terms' => $collection->slug, // Reemplaza 'nombre_del_termino' por el término específico de la taxonomía que deseas consultar
+            ),
+        ),
 
-                        <?php while($products->have_posts()): $products->the_post(); ?>
-                            <?php 
-                                $main_image=get_field("imagenes");
-                                $productName=get_field('nombre_producto');
-                                $details = get_field("descripcion");
-                            ?>
-                            <div class="col s12 m3">
-                                <div class="card center-align">
-                                    <div class="card-img">
-                                        <a href="<?php echo get_permalink() ?>">
-                                            <img class="" src="<?php echo $main_image['imagen1']['url'] ?>" alt="">
-                                    
-                                        </a>
-                                    </div>
-                                    <div class="card-content">
-                                        <?php if($productName): ?>
-                                            <a href="<?php echo get_permalink() ?>">
-                                                <h5><?php echo $productName ?></h5>
-                                                <h4>Colección <?php echo $collection->name ?></h4>
-                                            </a>
-                                        <?php endif; ?>
-                                    </div>
+    );
+    $products = new WP_Query($args);
+
+    ?>
+    <?php if ($products->have_posts()): ?>
+        <div class="">
+            <div class="flex justify-center">
+                <div class="flex flex-wrap gap-[25px]">
+
+                    <?php while ($products->have_posts()):
+                        $products->the_post(); ?>
+                        <?php
+                        $main_image = get_field("imagenes");
+                        $productName = get_field('nombre_producto');
+                        $details = get_field("descripcion");
+                        ?>
+                        <div class="min-w-[250px] max-w-[20%]">
+                            <div class="flex flex-col">
+                                <div class="">
+                                    <a href="<?php echo get_permalink() ?>">
+                                        <img class="responsive-img" src="<?php echo $main_image['imagen1']['url'] ?>" alt="">
+
+                                    </a>
                                 </div>
-        
+                                <div class="py-[10px] px[20px] flex flex-col gap-[20px]">
+                                    <?php if ($productName): ?>
+                                        <a href="<?php echo get_permalink() ?>">
+                                            <h5><?php echo $productName ?></h5>
+                                        </a>
+                                    <?php endif; ?>
+                                </div>
                             </div>
-                        <?php endwhile ?>
-                        <?php wp_reset_postdata() ?>
-                    </div>
 
+                        </div>
+                    <?php endwhile ?>
+                    <?php wp_reset_postdata() ?>
                 </div>
+
             </div>
-        <?php else: ?>
+        </div>
+    <?php else: ?>
         <p>no se encontro nada</p>
     <?php endif ?>
 
